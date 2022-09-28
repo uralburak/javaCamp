7b2ebd24fd27f7c65204947f3269b74101366db9
Burak Ural <77548008+uralburak@users.noreply.github.com>
2022-09-28 04:17:23 +0300

    Week 2

---
+++ classesWithAttributes/src/classesWithAttributes/Product.java
@@ -0,0 +1,69 @@
+package classesWithAttributes;
+
+public class Product {
+	
+	//ayný isimle farklý parametrelrle methodlarýmýzý kullanabiliyoruz.
+	public Product(int id, String name, String description, double price, int stockAmount ){  //constructor videosu
+		
+		System.out.println("Yapýcý blok çalýþtý.");
+		
+	}
+	
+	public Product() {
+		
+	}
+	
+	//attribute | field
+	private String name;
+	private String description;
+	private int id;
+	private double price;
+	private int stockAmount;
+	private String renk;
+	private String kod;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getStockAmount() {
+		return stockAmount;
+	}
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+	public String getRenk() {
+		return renk;
+	}
+	public void setRenk(String renk) {
+		this.renk = renk;
+	}
+	public String getKod() {
+		return this.name.substring(0,1) + id;
+	}
+	public void setKod(String kod) {
+		this.kod = kod;
+	}
+
+
+
+}
